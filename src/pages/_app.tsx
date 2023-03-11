881166107e3a1f098ad20dcafd189ab2966e9661
Rengga Prakoso Nugroho <rengga.prakoso@gmail.com>
2023-03-11 12:57:59 +0700

    add layer security on admin path using edtechID

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import { SessionProvider } from "next-auth/react"
 import '@/styles/globals.css'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 
@@ -5,7 +6,15 @@ import { config } from '@fortawesome/fontawesome-svg-core'
 config.autoAddCss = false
 
 import type { AppProps } from 'next/app'
+import type { Session } from "next-auth"
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps<{ session: Session }>) {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  )
 }

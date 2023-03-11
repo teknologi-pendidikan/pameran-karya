aec083a7a3146600b6e338be72b301b9e8a4d598
Rengga Prakoso Nugroho <rengga.prakoso@gmail.com>
2023-03-11 16:41:45 +0700

    migrate to CSR using SWR

--- src/pages/admin/daftarPeserta.tsx
+++ src/pages/admin/daftarPeserta.tsx
@@ -1,18 +1,30 @@
-/* eslint-disable react/jsx-key */
-// @ts-nocheck
+import useSWR from "swr";
 
-export default function DaftarPeserta({ daftarpeserta }) {  
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
+export type pesertaDataModel = {
+  uuid: string;
+  nim: number;
+  nama: string;
+};
+
+export default function DaftarPeserta() {
+  const { data, error, isLoading } = useSWR("/api/daftarpeserta", fetcher);
+
+  if (error) return <div>failed to load</div>;
+  if (isLoading) return <div>loading...</div>;
+
   return (
     <>
       <h1 className="text-xl">Daftar Peserta</h1>
       <table className="table-auto border-collapse border border-slate-500">
         <tr>
-          <th className="border border-slate-600" >UUID</th>
+          <th className="border border-slate-600">UUID</th>
           <th className="border border-slate-600">NIM</th>
           <th className="border border-slate-600">Nama</th>
         </tr>
-        {daftarpeserta.map((peserta) => (
-          <tr>
+        {data.map((peserta: pesertaDataModel) => (
+          <tr key={peserta.uuid}>
             <td className="border border-slate-600">{peserta.uuid}</td>
             <td className="border border-slate-600">{peserta.nim}</td>
             <td className="border border-slate-600">{peserta.nama}</td>
@@ -21,15 +33,4 @@ export default function DaftarPeserta({ daftarpeserta }) {
       </table>
     </>
   );
-}
-
-export async function getServerSideProps() {
-  const res = await fetch("http://localhost:3000/api/daftarpeserta");
-  const daftarpeserta = await res.json();
-
-  return {
-    props: {
-      daftarpeserta,
-    },
-  };
 }

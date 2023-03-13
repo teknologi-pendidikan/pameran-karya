33fa3a63714591fb267ea3cd7f2c1d2aa3b21667
Rengga Prakoso Nugroho <rengga.prakoso@gmail.com>
2023-03-13 23:54:35 +0700

    fixes for smaller screen size layout

--- src/components/section/Header.tsx
+++ src/components/section/Header.tsx
@@ -4,48 +4,44 @@ import Image from "next/image";
 
 function Header() {
   return (
-    <header className="hidden 2xl:block transition-all ease-in-out pt-4 z-50 absolute left-0 top-0 w-full font-PlusJakartaSans">
-      <nav className="max-w-4xl px-4 sm:px-6 lg:px-8" aria-label="Top">
-        <div className="flex w-full items-center justify-around space-x-16">
-          <div className="flex items-center">
-            <Link href="/">
-              <Image
-                className="h-20 w-auto"
-                src="/logo-pameran.svg"
-                alt="Logo Pameran TEP 2023"
-                width={200}
-                height={100}
-              />
-            </Link>
-          </div>
-          <div className="hidden space-x-8 lg:block">
-            <Link
-              href="/about"
-              className="text-base font-medium text-white hover:text-gray-200 hover:underline underline-offset-4"
-            >
-              About
-            </Link>
-            <Link
-              href="/speakers"
-              className="text-base font-medium text-white hover:text-gray-200 hover:underline underline-offset-4"
-            >
-              Participants
-            </Link>
-            <Link
-              href="/schedule"
-              className="text-base font-medium text-white hover:text-gray-200 hover:underline underline-offset-4"
-            >
-              Teams
-            </Link>
-            <Link
-              href="/sponsors"
-              className="text-base font-medium text-white hover:text-gray-200 hover:underline underline-offset-4"
-            >
-              Virtual Exhibitions
-            </Link>
-          </div>
-          <div className="ml-10 hidden space-x-4 md:block"></div>
+    <header className="my-4 z-50 absolute top-0 w-full font-PlusJakartaSans">
+      <nav className="flex flex-col items-center">
+        <Link href="/">
+          <Image
+            className=""
+            src="/logo-pameran.svg"
+            alt="Logo Pameran TEP 2023"
+            width={300}
+            height={100}
+          />
+        </Link>
+        <div className="space-x-8">
+          <Link
+            href="/about"
+            className="text-base font-medium text-white hover:text-gray-200 hover:underline underline-offset-4"
+          >
+            Tentang
+          </Link>
+          <Link
+            href="/speakers"
+            className="text-base font-medium text-white hover:text-gray-200 hover:underline underline-offset-4"
+          >
+            Partisipan
+          </Link>
+          <Link
+            href="/schedule"
+            className="text-base font-medium text-white hover:text-gray-200 hover:underline underline-offset-4"
+          >
+            Tim Belakang Layar
+          </Link>
+          <Link
+            href="/sponsors"
+            className="text-base font-medium text-white hover:text-gray-200 hover:underline underline-offset-4"
+          >
+            Butuh bantuan?
+          </Link>
         </div>
+        <div className="ml-10 hidden space-x-4 md:block"></div>
       </nav>
     </header>
   );

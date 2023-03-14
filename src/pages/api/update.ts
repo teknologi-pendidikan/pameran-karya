4e849fca1f76a88dbc9e66bf5f7fb4c4ad0b236d
Rengga Prakoso Nugroho <rengga.prakoso@gmail.com>
2023-03-14 23:40:47 +0700

    updatecontent API calls the revalidate on specific key

--- src/pages/api/update.ts
+++ src/pages/api/update.ts
@@ -3,35 +3,44 @@ import { NextApiRequest, NextApiResponse } from 'next'
 const fetch = require('cross-fetch')
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  // Get data submitted in request's body.
   const body = req.body
 
-  // Optional logging to see the responses
-  // in the command line where next.js app is running.
-  console.log('body: ', body)
-
-  // Guard clause checks for first and last name,
-  // and returns early if they are not found
   if (!body.reason || !body.key) {
-    // Sends a HTTP bad request error code
     return res.status(400).json({
       error: 'Missing reason or key'
     })
   }
 
-  const WEBHOOK_URL = `https://api.netlify.com/build_hooks/640edb1876710f35d5103388?trigger_title=${body.key}::${body.reason}`
+  if (body.key !== process.env.REVALIDATE_SECRET) {
 
-  fetch(WEBHOOK_URL, {
-    method: 'POST'
-  })
+    const WEBHOOK_URL = `https://api.netlify.com/build_hooks/640edb1876710f35d5103388?trigger_title=${body.key}::${body.reason}`
 
-  // Sends a HTTP success code
-  return res.status(200).json({
-    success: true,
-    reason: body.reason,
-    key: body.key
+    fetch(WEBHOOK_URL, {
+      method: 'POST'
+    })
 
-  })
+    // Sends a HTTP success code
+    return res.status(200).json({
+      mode: 'build',
+      reason: body.reason,
+      key: body.key,
+      success: true
+
+    })
+  } else {
+    // Sends a HTTP success code
+    fetch(`${process.env.URL}/api/revalidate?key=${process.env.REVALIDATE_SECRET}`, {
+      method: 'GET',
+    }).then((response) => response.json())
+      .then((json) => {
+        return res.status(200).json({
+          mode: 'revalidate',
+          reason: body.reason,
+          key: body.key,
+          success: json.revalidated
+        })
+      })
+  }
 }
 
 export const config = {

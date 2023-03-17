2f680eff5040350c875d9b36c2f9f6995a8ee2f0
Rengga Prakoso Nugroho <rengga.prakoso@gmail.com>
2023-03-17 21:36:03 +0700

    revert to absolute header position

--- src/components/section/Header.tsx
+++ src/components/section/Header.tsx
@@ -6,15 +6,15 @@ interface HeaderProps {
   position: string;
 }
 
-function Header({position = "absolute"}: HeaderProps) {
+function Header({position}: HeaderProps) {
   return (
-    <header className={`my-4 z-50 xl:${position} xl:top-0 w-full font-PlusJakartaSans`}>
+    <header className={`my-4 z-50 xl:absolute xl:top-0 w-full font-PlusJakartaSans`}>
       <nav className="flex flex-col items-center">
         <Link href="/">
           <h1 className="hidden">Pameran Karya Teknologi Pendidikan 2023</h1>
           <LogoPameran />
         </Link>
-        <div className="mb-4 block space-x-8">
+        <div className="mb-4 hidden 2xl:block space-x-8">
           <Link
             href="/about"
             className="text-base font-medium text-white hover:text-gray-200 hover:underline underline-offset-4"
@@ -40,7 +40,6 @@ function Header({position = "absolute"}: HeaderProps) {
             Butuh bantuan?
           </Link>
         </div>
-        <div className="ml-10 hidden space-x-4 md:block"></div>
       </nav>
     </header>
   );

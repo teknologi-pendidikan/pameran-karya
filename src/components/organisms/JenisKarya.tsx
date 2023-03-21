1d16288fb00a19a185191d3d2b19b52e2ad1f634
Rengga Prakoso Nugroho <rengga.prakoso@gmail.com>
2023-03-22 00:47:05 +0700

    feat: new jenis karya section external links

---
+++ src/components/organisms/JenisKarya.tsx
@@ -0,0 +1,41 @@
+import FrameChara from "../../../public/frame-chara.png";
+import Image from "next/image";
+import SectionTitle from "@components/atoms/section-title";
+
+export default function JenisKarya() {
+  return (
+    <div className="container mx-auto mt-28">
+        <SectionTitle title="Kategori Karya Pameran" />
+      <div className="flex flex-row items-center justify-center space-x-4">
+        <Image
+          src={FrameChara}
+          alt="frame-chara"
+          priority
+          quality={50}
+          className="grow-animation saturate-50 hover:scale-110 hover:saturate-100"
+        />
+        <Image
+          src={FrameChara}
+          alt="frame-chara"
+          priority
+          quality={50}
+          className="grow-animation saturate-50 hover:scale-110 hover:saturate-100"
+        />
+        <Image
+          src={FrameChara}
+          alt="frame-chara"
+          priority
+          quality={50}
+          className="grow-animation saturate-50 hover:scale-110 hover:saturate-100"
+        />
+        <Image
+          src={FrameChara}
+          alt="frame-chara"
+          priority
+          quality={50}
+          className="grow-animation saturate-50 hover:scale-110 hover:saturate-100"
+        />
+      </div>
+    </div>
+  );
+}

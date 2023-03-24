edc970b2d293b002e0596b296a4600c720bc931c
Rengga Prakoso Nugroho <rengga.prakoso@gmail.com>
2023-03-24 13:29:14 +0700

    feat: added basic Security Header and loose CSP

--- next.config.js
+++ next.config.js
@@ -1,8 +1,15 @@
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self' 'unsafe-inline' 'unsafe-eval';
+  style-src 'self' 'unsafe-inline';
+  font-src 'self' ;  
+`;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    remotePatterns: [ 
+    remotePatterns: [
       {
         protocol: "https",
         hostname: "api.netlify.com",
@@ -13,6 +20,45 @@ const nextConfig = {
       },
     ],
   },
-}
+  async headers() {
+    return [
+      {
+        source: "/(.)*",
+        headers: [
+          {
+            key: "X-XSS-Protection",
+            value: "1; mode=block",
+          },
+          {
+            key: "X-Frame-Options",
+            value: "SAMEORIGIN",
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "Referrer-Policy",
+            value: "strict-origin-when-cross-origin",
+          },
+          {
+            key: "Permissions-Policy",
+            value:
+              "camera=(), microphone=(), geolocation=(), interest-cohort=()",
+          },
+          {
+            key: "Content-Security-Policy",
+            value: ContentSecurityPolicy.replace(/\s{2,}/g, " ").trim(),
+          },
+          { key: "X-DNS-Prefetch-Control", value: "on" },
+          { key: "X-Download-Options", value: "noopen" },
+          { key: "X-Permitted-Cross-Domain-Policies", value: "none" },
+          { key: "X-EdtechID-App", value: "Pameran Karya Digital 2023" },
+          { key: "X-EdtechID-Unit", value: "DPTSI-UM-2023" },
+        ],
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;

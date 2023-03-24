941b4cfb8a2b47f3387944110fb5fc3bf14fcf57
Rengga Prakoso Nugroho <rengga.prakoso@gmail.com>
2023-03-24 16:54:39 +0700

    fix: make link generate programtically

--- src/components/organisms/Navbar.tsx
+++ src/components/organisms/Navbar.tsx
@@ -7,42 +7,56 @@ type NavbarProps = {
   extendRootNavbar?: string;
 };
 
+const NAVLINK = [
+  {
+    name: "Tentang",
+    link: "/tentang",
+  },
+  {
+    name: "Partisipan",
+    link: "/partisipan",
+  },
+  {
+    name: "Karya",
+    link: "/karya",
+  },
+  {
+    name: "Panduan",
+    link: "/panduan",
+  },
+  {
+    name: "Butuh bantuan?",
+    link: "/bantuan",
+  },
+];
+
 function Navbar(Navbar: NavbarProps) {
   return (
-    <nav
-      className={`${Navbar.extendRootNavbar} z-10 flex flex-row items-center py-1 w-full font-PlusJakartaSans px-12 mx-auto`}
+    <div
+      id="navbar-wrapper"
+      className={`w-full ${Navbar.extendRootNavbar} z-10 flex justify-center`}
     >
-      <Link href="/">
-        <h1 className="hidden">Pameran Karya Teknologi Pendidikan 2023</h1>
-        <LogoPameran width={200} height={75} />
-      </Link>
-      <div className="px-8 hidden 2xl:block space-x-8 items-center">
-        <Link
-          href="/tentang"
-          className="text-base font-medium text-white hover:text-gray-200 hover:underline underline-offset-4"
-        >
-          Tentang
+      <nav
+        id="navbar"
+        className="flex flex-row items-center justify-between space-x-1 container"
+      >
+        <Link href="/">
+          <h1 className="hidden">Pameran Karya Teknologi Pendidikan 2023</h1>
+          <LogoPameran width={200} height={75} />
         </Link>
-        <Link
-          href="/partisipan"
-          className="text-base font-medium text-white hover:text-gray-200 hover:underline underline-offset-4"
-        >
-          Partisipan
-        </Link>
-        <Link
-          href="/karya"
-          className="text-base font-medium text-white hover:text-gray-200 hover:underline underline-offset-4"
-        >
-          karya
-        </Link>
-        <Link
-          href="/bantuan"
-          className="text-base font-medium text-white hover:text-gray-200 hover:underline underline-offset-4"
-        >
-          Butuh bantuan?
-        </Link>
-      </div>
-    </nav>
+        <div className="hidden 2xl:block space-x-8 items-center">
+          {NAVLINK.map((link) => (
+            <Link
+              key={link.link}
+              href={link.link}
+              className="text-base font-medium text-white hover:text-gray-200 hover:underline underline-offset-4"
+            >
+              {link.name}
+            </Link>
+          ))}
+        </div>
+      </nav>
+    </div>
   );
 }
 

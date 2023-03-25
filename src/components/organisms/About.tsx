af27477d19edbe1da4278112bba29df84e110957
Rengga Prakoso Nugroho <rengga.prakoso@gmail.com>
2023-03-26 02:32:36 +0700

    fix: give thumbnail in youtube video for light loads

--- src/components/organisms/About.tsx
+++ src/components/organisms/About.tsx
@@ -46,33 +46,13 @@ export default function About() {
               karya-karya yang tidak hanya mencengangkan, tetapi juga dapat
               mengubah paradigma pendidikan yang sudah ada.
             </p>
-
-            {/* <div className="space-y-3">
-              <div className="flex items-center space-x-3">
-                <div className="flex justify-center"></div>
-                <FontAwesomeIcon icon={faCalendarAlt} />
-                <span className="text-base">Oct - Dec, 2022</span>
-              </div>
-              <div className="flex items-center space-x-3">
-                <div className="flex justify-center"></div>
-                <FontAwesomeIcon icon={faMapMarkerAlt} />
-                <span className="text-base">In-person / Hybrid</span>
-              </div>
-            </div>
-            <div className="flex flex-col md:flex-row items-center space-y-3 md:space-y-0 md:space-x-6">
-              <Link
-                // text="Find DevFest near you"
-                className="w-full sm:w-5/6 md:w-auto justify-between"
-                href="https://gdg.community.dev/events/"
-              />
-            </div> */}
           </div>
           <div className="w-full">
             <div className="rounded-lg overflow-hidden">
               <ReactPlayer
                 width="100%"
                 url="https://www.youtube.com/watch?v=EZ_DPwBU4h8"
-                light="true"
+                light={<img src="/placeholder.png" alt="hero" />}
               />
             </div>
           </div>

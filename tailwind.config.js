1199a620332e331d4bfc1845290eaf1dd3e3f7b6
Rengga Prakoso Nugroho <rengga.prakoso@gmail.com>
2023-03-28 03:44:11 +0700

    feat: added event color scheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,17 @@ module.exports = {
         display: ['"Plus Jakarta Sans"', "sans-serif"],
         body: ['"Plus Jakarta Sans"', "sans-serif"],
       },
+      colors: {
+        theme: {
+          flower: "#D9A3D5",
+          white: "#F2F2F2",
+          violet: "#7165BF",
+          blue: {
+            accent: "#3C41A6",
+            dark: "#1C31A6",
+          },
+        },
+      },
     },
   },
   plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography")],

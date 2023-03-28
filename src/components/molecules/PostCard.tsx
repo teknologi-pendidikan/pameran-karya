4dd3b102ba09fc26c6b2310cbb6ba998e31a8a9a
Rengga Prakoso Nugroho <rengga.prakoso@gmail.com>
2023-03-29 04:04:59 +0700

    chore: remove console output

--- src/components/molecules/PostCard.tsx
+++ src/components/molecules/PostCard.tsx
@@ -9,7 +9,6 @@ type PostCardProps = {
 };
 
 export default function PostCard({ link, title, image }: PostCardProps) {
-  console.log(title);
   return (
     <div className="border border-theme-flower rounded-md shadow-lg overflow-hidden flex flex-col transition ease-in-out duration-500 hover:scale-110">
       <Link href={`/post/${link}`}>

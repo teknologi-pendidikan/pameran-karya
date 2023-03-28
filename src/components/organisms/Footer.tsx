3185b885daf78d2ec9ad7f7afac660affea22446
Rengga Prakoso Nugroho <rengga.prakoso@gmail.com>
2023-03-28 14:53:46 +0700

    chore: defer p tag element and use span instead

--- src/components/organisms/Footer.tsx
+++ src/components/organisms/Footer.tsx
@@ -5,17 +5,17 @@ import FooterSponsor from "../molecules/footer-sponsor";
 
 export default function Footer() {
   return (
-    <footer className="relative w-full bg-black h-screen" aria-label="">
+    <footer className="relative w-full bg-black min-h-min pb-10" aria-label="">
       <div className="mx-auto container px-4">
         <div className="flex-row">
           <div className="flex flex-col justify-between md:flex-row md:items-center pt-10 md:pt-16">
             <div className="flex flex-col justify-center scale-75 md:scale-100">
               <LogoPameran />
-              <p className="text-white text-[20px] md:text-[24px] md:flex md:flex-wrap">
+              <span className="text-white text-[20px] md:text-[24px] md:flex md:flex-wrap">
                 Inovasi. Teknologi. Pendidikan.
-              </p>
+              </span>
             </div>
-            <div className="flex flex-col md:flex-row space-y-5 md:space-y-0 space-x-0 md:space-x-6 lg:space-x-[60px] items-start justify-center mx-auto md:mx-0 mt-10 md:mt-0">
+            <div className="hidden md:flex flex-col md:flex-row space-y-5 md:space-y-0 space-x-0 md:space-x-6 lg:space-x-[60px] items-start justify-center mx-auto md:mx-0 mt-10 md:mt-0">
               <FooterPrevent />
               <FooterMainevent />
             </div>
@@ -23,8 +23,8 @@ export default function Footer() {
           <FooterSponsor />
         </div>
         <hr className="h-[1px] bg-white md:bg-discolored-800 mt-10 md:mt-[67px]" />
-        <p className="text-white font-semibold text-[14px] md:text-[16px] mt-3 pb-3 md:mt-5 md:pb-10">
-          © 2023 Pameran Karya Digital TEP UM. All rights reserved.
+        <p className="text-white font-semibold text-[14px] md:text-[16px] mt-3 pb-3 md:mt-5 md:pb-10 text-center md:text-right">
+          © 2023 Pameran Karya Digital TEP UM. <br /> All rights reserved.
         </p>
       </div>
     </footer>

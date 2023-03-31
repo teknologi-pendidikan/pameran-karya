a95151949e1d69f12dc5a009c9fb4b13bbfca40f
Rengga Prakoso Nugroho <rengga.prakoso@gmail.com>
2023-03-31 22:26:22 +0700

    perf: make the selector programtically created

--- src/components/organisms/section/PlayerSelector.tsx
+++ src/components/organisms/section/PlayerSelector.tsx
@@ -3,8 +3,26 @@ import Player_2 from "@/public/assets/img-jeniskarya.webp";
 import Image from "next/image";
 import Slider from "react-slick";
 import SectionLayout from "@/components/template/section-layout";
+import Link from "next/link";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+
+const PLAYERLIST = [
+  {
+    id: "player-partisipan",
+    title: "Partisipan Pameran",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem, quia.",
+    link: "/post/pedoman-partisipan",
+    img: Player_1,
+  },
+  {
+    id: "player-pengunjung",
+    title: "Pengunjung Pameran",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem, quia.",
+    link: "/post/pedoman-pengunjung",
+    img: Player_2,
+  },
+];
 
 export default function PlayerSelector() {
   const sliderSetting = {
@@ -25,29 +43,19 @@ export default function PlayerSelector() {
       {/* Player Selector Desktop Mode */}
       <div
         id="player-selector"
-        className="hidden xl:flex flex-row justify-between items-center"
+        className="hidden xl:flex flex-row justify-around items-center"
       >
-        <div
-          id="player-partisipan"
-          className="text-3xl text-center saturate-0 hover:scale-110 hover:saturate-100 transition ease-in-out duration-500"
-        >
-          <Image src={Player_1} alt="player_1" />
-          <h3>Partsipan Pameran</h3>
-        </div>
-        <div
-          id="player-partisipan"
-          className="text-3xl text-center saturate-0 hover:scale-110 hover:saturate-100 transition ease-in-out duration-500"
-        >
-          <Image src={Player_1} alt="player_1" />
-          <h3>Partsipan Pameran</h3>
-        </div>
-        <div
-          id="player-pengunjung"
-          className="text-3xl text-center saturate-0 hover:scale-110 hover:saturate-100 transition ease-in-out duration-500"
-        >
-          <Image src={Player_2} alt="player_1" />
-          <h3>Pengunjung Pameran</h3>
-        </div>
+        {PLAYERLIST.map((player) => (
+          <div
+            key={player.id}
+            id={player.id}
+            className="text-3xl text-center saturate-0 hover:scale-110 hover:saturate-100 transition ease-in-out duration-500"
+          >
+            <Link href={player.link} aria-label={player.title}>
+              <Image src={player.img} alt={player.id} />
+            </Link>
+          </div>
+        ))}
       </div>
 
       {/* Player Selector Mobile Nav */}

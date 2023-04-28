8977eca6bbdbacd69fbfe2a7fc4efcaa1b49b848
Rengga Prakoso Nugroho <rengga.prakoso@gmail.com>
2023-04-29 00:11:54 +0700

    feat: added role tag and uploaded karya

--- src/components/molecules/profile-card.tsx
+++ src/components/molecules/profile-card.tsx
@@ -7,6 +7,8 @@ interface ProfileCardProps {
   image: string;
   description: string;
   uuid: string;
+  role: string;
+  totalKarya: string;
 }
 
 export default function ProfileCard(
@@ -14,6 +16,60 @@ export default function ProfileCard(
 ): JSX.Element {
   // Fall back to the default image if the image is not found
   const [imgSrc, setImgSrc] = useState(profileInfo.image);
+
+  let roleTag = "bg-green-400";
+
+  if (profileInfo.role === "Ketua") {
+    roleTag = "bg-red-500";
+  }
+
+  if (profileInfo.role === "Wakil Ketua") {
+    roleTag = "bg-red-500";
+  }
+
+  if (profileInfo.role === "Kurator") {
+    roleTag = "bg-blue-500";
+  }
+
+  if (profileInfo.role === "System Engineer") {
+    roleTag = "bg-neutral-700";
+  }
+
+  if (profileInfo.role === "Datalake") {
+    roleTag = "bg-fuschia-500";
+  }
+
+  if (profileInfo.role === "Bughunter") {
+    roleTag = "bg-amber-500";
+  }
+
+  if (profileInfo.role === "Virtual Exhibition") {
+    roleTag = "bg-violet-600";
+  }
+
+  if (profileInfo.role === "Design") {
+    roleTag = "bg-cyan-500";
+  }
+
+  if (profileInfo.role === "Logistic") {
+    roleTag = "bg-purple-500";
+  }
+
+  if (profileInfo.role === "Documentation") {
+    roleTag = "bg-sky-500";
+  }
+
+  if (profileInfo.role === "Events") {
+    roleTag = "bg-orange-500";
+  }
+
+  if (profileInfo.role === "Public Relation") {
+    roleTag = "bg-indigo-500";
+  }
+
+  if (profileInfo.role === "Fundraiser") {
+    roleTag = "bg-rose-500";
+  }
 
   useEffect(() => {
     setImgSrc(profileInfo.image);
@@ -45,8 +101,13 @@ export default function ProfileCard(
             >
               {profileInfo.name}
             </p>
-            <div className="text-gray-500 text-sm space-y-1">
-              <span>{profileInfo.title}</span>
+            <div className={`text-sm px-4 rounded-full py-1`}>
+              <span>{`Karya terunggah: ${profileInfo.totalKarya}`}</span>
+            </div>
+            <div
+              className={` ${roleTag} text-sm px-4 bg-indigo-700 rounded-full py-1 text-white`}
+            >
+              <span>{profileInfo.role}</span>
             </div>
           </div>
         </div>

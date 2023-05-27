e54e67aea9e453d3ec1c61c29dc81b60448ba50d
Rengga Prakoso Nugroho <rengga.prakoso@gmail.com>
2023-05-27 13:34:41 +0700

    fix: disable optimization for the entire image file
    Due to heavy netlify ipx usage 
    
    Signed-off-by: Rengga Prakoso Nugroho <rengga.prakoso@gmail.com>

--- next.config.js
+++ next.config.js
@@ -23,6 +23,7 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
 const nextConfig = {
   reactStrictMode: true,
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",

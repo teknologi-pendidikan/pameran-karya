3eab25435cedbebb4ece61f00e1bd4142796e542
vzrenggamani <rengganugroho@teknologipendidikan.or.id>
2025-12-21 03:10:32 +0700

    dx: basic prettier and eslint

---
+++ .lintstagedrc.js
@@ -0,0 +1,11 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
+const path = require("path");
+
+const buildEslintCommand = (filenames) =>
+  `eslint --fix ${filenames
+    .map((f) => `"${path.relative(process.cwd(), f)}"`)
+    .join(" ")}`;
+
+module.exports = {
+  "*.{js,jsx,ts,tsx}": [buildEslintCommand],
+};

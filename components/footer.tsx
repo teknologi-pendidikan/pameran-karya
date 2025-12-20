fb9ae009c80c00528b7700818c7662d3c61a1ad8
vzrenggamani <rengganugroho@teknologipendidikan.or.id>
2025-12-21 04:10:20 +0700

    ui: smaller logo on mobile

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,8 +2,8 @@ import LogoPameran from "@/assets/logo_pameran";
 
 export default function Footer() {
   return (
-    <div className="bg-base-200 pt-12 pb-16">
-      <footer className="container mx-auto max-w-7xl footer sm:footer-horizontal  text-base-content p-10">
+    <div className="bg-base-200 pt-12 pb-16 mt-16">
+      <footer className="container mx-auto max-w-7xl footer footer-vertical sm:footer-horizontal px-4 text-base-content">
         <nav>
           <h6 className="footer-title">Tentang Kami</h6>
           <a className="link link-hover">Whitepaper</a>
@@ -22,18 +22,24 @@ export default function Footer() {
           <a className="link link-hover">Kebijakan kurasi</a>
           <a className="link link-hover">Layanan Sanggahan</a>
         </nav>
+        <nav>
+          <h6 className="footer-title">Teknologi</h6>
+          <a className="link link-hover">Fork our codebase</a>
+          <a className="link link-hover">Kebijakan opensource</a>
+          <a className="link link-hover">Security disclosure</a>
+        </nav>
       </footer>
-      <footer className=" container mx-auto max-w-7xl footer text-base-content border-base-300 border-t px-10 py-4">
-        <aside className="grid-flow-row lg:grid-col items-center">
+      <footer className="container mx-auto max-w-7xl footer footer-vertical sm:footer-horizontal text-base-content border-base-300 border-t px-4 py-8 mt-8">
+        <aside className="flex flex-col sm:flex-row items-center sm:items-start space-y-2 sm:space-y-0 sm:space-x-4">
           <LogoPameran
-            className="h-10 w-auto"
-            style={{ height: "auto", width: "256px" }}
+            className="h-8 sm:h-10 w-auto max-w-48 sm:max-w-64"
+            style={{ height: "auto", width: "100%", maxWidth: "256px" }}
             fillcolorbottom="black"
             fillcolortop="black"
           />
-          <p className="mx-0 lg:mx-4 text-sm">
-            by Teknologi Pendidikan ID
-            <br />& Ikatan Mahasiswa Teknologi Pendidikan Indonesia
+          <p className="text-center sm:text-left text-sm lg:max-w-xs">
+            by Teknologi Pendidikan ID & Ikatan Mahasiswa Teknologi Pendidikan
+            Indonesia
           </p>
         </aside>
       </footer>

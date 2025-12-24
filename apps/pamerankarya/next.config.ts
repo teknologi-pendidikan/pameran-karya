4f86dfb93b43d0c55daddce9fe4b9135ba7c5d0c
vzrenggamani <rengganugroho@teknologipendidikan.or.id>
2025-12-25 02:51:50 +0700

    chore: update security headers

--- apps/pamerankarya/next.config.ts
+++ apps/pamerankarya/next.config.ts
@@ -1,6 +1,21 @@
 import type { NextConfig } from "next";
 import createMDX from "@next/mdx";
 
+const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline' https://www.googletagmanager.com https://www.google-analytics.com https://cdn.jsdelivr.net/;
+    style-src 'self' 'unsafe-inline' https://cdn.jsdelivr.net/;
+    img-src 'self' blob: data: https://i.ytimg.com/ https://img.youtube.com/;
+    frame-src https://www.youtube.com/ https://www.google.com/ https://www.youtube-nocookie.com/ https://docs.google.com/ https://calendar.google.com/;
+    font-src 'self';
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    upgrade-insecure-requests;
+    block-all-mixed-content;
+`;
+
 const withMDX = createMDX({
   extension: /\.mdx?$/,
 });
@@ -8,6 +23,39 @@ const withMDX = createMDX({
 const nextConfig: NextConfig = {
   pageExtensions: ["ts", "tsx", "md", "mdx"],
   output: "export",
+  headers: async () => {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: cspHeader.replace(/\s{2,}/g, " ").trim(),
+          },
+          {
+            key: "Referrer-Policy",
+            value: "strict-origin-when-cross-origin",
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "X-Frame-Options",
+            value: "DENY",
+          },
+          {
+            key: "X-DNS-Prefetch-Control",
+            value: "on",
+          },
+          {
+            key: "Permissions-Policy",
+            value: "camera=(), microphone=(), geolocation=()",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default withMDX(nextConfig);

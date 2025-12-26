37b8e6cebc2dcfdd7ef89841715f74c8daf003ed
vzrenggamani <rengganugroho@teknologipendidikan.or.id>
2025-12-27 03:24:17 +0700

    feat: make login using google

--- apps/backoffice/app/auth/page.tsx
+++ apps/backoffice/app/auth/page.tsx
@@ -1,14 +1,11 @@
-import { login, signup } from "@/app/auth/actions";
+import { LoginForm } from "@/components/login-form";
 
-export default function LoginPage() {
+export default function Page() {
   return (
-    <form>
-      <label htmlFor="email">Email:</label>
-      <input id="email" name="email" type="email" required />
-      <label htmlFor="password">Password:</label>
-      <input id="password" name="password" type="password" required />
-      <button formAction={login}>Log in</button>
-      <button formAction={signup}>Sign up</button>
-    </form>
+    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm">
+        <LoginForm />
+      </div>
+    </div>
   );
 }
--- apps/backoffice/components/login-form.tsx
+++ apps/backoffice/components/login-form.tsx
@@ -27,7 +27,7 @@ export function LoginForm({
 
     try {
       const { error } = await supabase.auth.signInWithOAuth({
-        provider: "github",
+        provider: "google",
         options: {
           redirectTo: `${window.location.origin}/auth/oauth?next=/dashboard`,
         },
@@ -52,7 +52,7 @@ export function LoginForm({
             <div className="flex flex-col gap-6">
               {error && <p className="text-sm text-destructive-500">{error}</p>}
               <Button type="submit" className="w-full" disabled={isLoading}>
-                {isLoading ? "Logging in..." : "Continue with GitHub"}
+                {isLoading ? "Logging in..." : "Continue with Google"}
               </Button>
             </div>
           </form>

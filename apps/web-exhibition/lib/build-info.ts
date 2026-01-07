8f1e4ae15777d3063e814c937fe13fac01b40019
vzrenggamani <rengganugroho@teknologipendidikan.or.id>
2026-01-07 21:37:58 +0700

    fix: csp add the sentry and img yt

--- apps/web-exhibition/lib/build-info.ts
+++ apps/web-exhibition/lib/build-info.ts
@@ -17,15 +17,15 @@ export interface BuildInfo {
 }
 
 const buildInfo: BuildInfo = {
-  "timestamp": "2026-01-02T13:28:53.003Z",
-  "date": "2 Januari 2026 pukul 20.28 WIB",
+  "timestamp": "2026-01-04T10:54:18.766Z",
+  "date": "4 Januari 2026 pukul 17.54 WIB",
   "version": "0.1.0",
   "nodeVersion": "v24.12.0",
-  "buildId": "wb95vu9tla",
+  "buildId": "frin0ym9zpj",
   "git": {
-    "commitHash": "e5ca98f56acebcc7561308a2cc571f8a4471551e",
-    "commitShort": "e5ca98f",
-    "commitMessage": "chore: remove obsolete components",
+    "commitHash": "d675cf7a12942b9c7fbf8632c31af6354e5e88a1",
+    "commitShort": "d675cf7",
+    "commitMessage": "feat: auto-detect the asset type based on link",
     "branchName": "2025",
     "gitlabUrl": "https://gitlab.com/teknologi-pendidikan/pamerankarya"
   }
